d0cd76775a9ea3337f63036d3503f41a33299d21
sicoding1120 <147905588+sicoding1120@users.noreply.github.com>
2023-12-02 14:31:02 +0700

    first commit

---
+++ asset/script/script.js
@@ -0,0 +1,13 @@
+const navbar = document.querySelector('.navbar');
+const pencet = document.querySelector('#tombol');
+const body = document.body;
+
+pencet.addEventListener('click', function () {
+    navbar.classList.toggle('active');
+});
+
+
+
+
+
+
